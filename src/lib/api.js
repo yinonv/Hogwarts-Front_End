134e8c88025fd52e07a1d875b765e4f264519780
Yinon Vahab <yinonv@gmail.com>
2020-01-12 00:58:53 +0200

    firebase works

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,34 +1,35 @@
 import axios from 'axios'
 
-// const url = 'http://localhost' 
-// const port = 5000
+const url = 'http://127.0.0.1' 
+const port = 2700
 
-// const getStudents = () => {
-//     return axios.get(`${url}:${port}/students`)
-// }
-// const getSkills = () => {
-//     return axios.get(`${url}:${port}/skills`)
-// }
-// const getCourses = () => {
-//     return axios.get(`${url}:${port}/courses`)
-// }
-// const addStudent = (student) => {
-//     console.log(JSON.stringify(student))
-//     return axios.post(`${url}:${port}/students/add`, JSON.stringify(student))
-// }
-
-const url = 'https://hogwarts-students.herokuapp.com'
 const getStudents = () => {
-    return axios.get(`${url}/students`)
+    return axios.get(`${url}:${port}/students`)
 }
 const getSkills = () => {
-    return axios.get(`${url}/skills`)
+    return axios.get(`${url}:${port}/skills`)
 }
 const getCourses = () => {
-    return axios.get(`${url}/courses`)
+    return axios.get(`${url}:${port}/courses`)
 }
 const addStudent = (student) => {
     console.log(JSON.stringify(student))
-    return axios.post(`${url}/students/add`, JSON.stringify(student))
+    return axios.post(`${url}:${port}/students/add`, JSON.stringify(student))
 }
+
+// const url = 'https://hogwarts-students.herokuapp.com'
+// const getStudents = () => {
+//     return axios.get(`${url}/students`)
+// }
+// const getSkills = () => {
+//     return axios.get(`${url}/skills`)
+// }
+// const getCourses = () => {
+//     return axios.get(`${url}/courses`)
+// }
+// const addStudent = (student) => {
+//     console.log(JSON.stringify(student))
+//     return axios.post(`${url}/students/add`, JSON.stringify(student))
+// }
+
 export { getStudents, getSkills, getCourses, addStudent }
